be31c7e6741676bed783e61a2cc0c1720d5cf6a0
raghavarulselvam <raghav.cse2024@citchenai.net>
2025-12-01 23:32:03 +0530

    Implement scroll to top on route change

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import { CartProvider } from './context/CartContext'
 import App from './App.jsx'
 import './index.css'
 
+import ScrollToTop from './components/ScrollToTop'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
+            <ScrollToTop />
             <CartProvider>
                 <App />
             </CartProvider>
